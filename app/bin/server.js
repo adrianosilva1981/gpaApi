f03e28fa3c21df5accd774fd2581b4b1ae745fe7
Adriano Silva <adriano.silva@semantix.ai>
2024-10-25 10:11:25 -0300

    sync

--- app/bin/server.js
+++ app/bin/server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 require('../tmp-data/import-data')
 
 const app = require('../app')
-const http = require('http')
+const http = require('http2')
 const httpServer = http.createServer(app)
 const port = process.env.PORT
 

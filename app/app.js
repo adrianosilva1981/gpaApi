8b67df4185903704f8e1da8bfd3d58d7613de02f
Adriano Silva <adriano.silva@semantix.ai>
2024-10-26 08:14:20 -0300

    ignore database.db

--- app/app.js
+++ app/app.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
@@ -6,11 +5,9 @@ const bodyParser = require('body-parser');
 // routes
 const index = require('./routes/index.route');
 const prizes = require('./routes/prizes.route');
-// const notifyRoute = require('./routes/notify');
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
-
 
 app.use('/', index);
 app.use('/prizes', prizes);
--- app/tmp-data/import-data.js
+++ app/tmp-data/import-data.js
@@ -5,15 +5,15 @@ const path = require("path");
 
 const importData = async () => {
   // in case to use with file to view data with a sgbd
-  const filePath = path.join(__dirname, 'database.db');
-  if (!fs.existsSync(filePath)) {
-    fs.writeFileSync(filePath, '', 'utf-8')
-  }
+  // const filePath = path.join(__dirname, 'database.db');
+  // if (!fs.existsSync(filePath)) {
+  //   fs.writeFileSync(filePath, '', 'utf-8')
+  // }
 
   const movies = [];
-  // const db = new sqlite3.Database(":memory:");
+  const db = new sqlite3.Database(":memory:");
   // to set database to file
-  const db = new sqlite3.Database(filePath);
+  // const db = new sqlite3.Database(filePath);
 
   return new Promise((resolve, reject) => {
     const csvFilePath = path.join(__dirname, 'movielist.csv');
